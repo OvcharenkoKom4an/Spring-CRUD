ae45c599c43c301b765347722705461cc1372955
OvcharenkoKom4an <sashaovch4@gmail.com>
2025-09-15 18:21:32 +0200

    REST Get by email & username

--- src/main/java/com/komasan/springcrud/controllers/UserController.java
+++ src/main/java/com/komasan/springcrud/controllers/UserController.java
@@ -40,7 +40,7 @@ public class UserController {
         List<UserResponse> responseDto = users
                 .stream()
                 .map(userMapper::toResponseDto)
-                .collect(Collectors.toList());
+                .toList();
         userAuditLogService.logAction("GET", "UserClass", null);
 
         log.info("{} Users was found", responseDto.size());
@@ -57,6 +57,32 @@ public class UserController {
         userAuditLogService.logAction("GET", "UserClass", id);
 
         log.info("User with id {} was created", id);
+        return ResponseEntity.ok(userMapper.toResponseDto(user));
+    }
+
+    @GetMapping("/username/{username}")
+    public ResponseEntity<UserResponse> getUserByUsername(@PathVariable String username) {
+        UserClass user = userService.getUserByUsername(username);
+        if(user == null) {
+            log.warn("User with name {} does not exist", username);
+            return ResponseEntity.notFound().build();
+        }
+        userAuditLogService.logAction("GET", "UserClass", user.getId());
+
+        log.info("User with name {} was found", username);
+        return ResponseEntity.ok(userMapper.toResponseDto(user));
+    }
+
+    @GetMapping("/email/{email}")
+    public ResponseEntity<UserResponse> getUserByEmail(@PathVariable String email) {
+        UserClass user = userService.getUserByEmail(email);
+        if(user == null) {
+            log.warn("User with email {} does not exist", email);
+            return ResponseEntity.notFound().build();
+        }
+        userAuditLogService.logAction("GET", "UserClass", user.getId());
+
+        log.info("User with email {} was created", email);
         return ResponseEntity.ok(userMapper.toResponseDto(user));
     }
 
--- src/main/java/com/komasan/springcrud/repository/UserRepository.java
+++ src/main/java/com/komasan/springcrud/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.komasan.springcrud.repository;
 
 import com.komasan.springcrud.user.UserClass;
+import org.springframework.data.domain.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserClass, Long > {
-
+public interface UserRepository extends JpaRepository<UserClass, Long> {
+    UserClass findByUsername(String username);
+    UserClass findByEmail(String email);
 }
